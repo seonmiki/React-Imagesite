d527ac2db08251a8b4a60aeef017b63149c9e95f
seonmin <seonmiki@student.42seoul.kr>
2024-05-11 16:55:30 +0900

    Refact: recoil store

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,44 +6,19 @@ import CommonNav from "@/components/common/navigation/CommonNav";
 import CommonFooter from "@/components/common/footer/CommonFooter";
 import Card from "./components/Card";
 import { CardDTO } from "./types/card";
-
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { imageData } from "@/recoil/selectors/imageSelector";
 
 function index() {
-    const [imgUrls, setImgUrls] = useState([]);
+    const imgSelector = useRecoilValue(imageData);
+    const [imgData, setImgData] = useState<CardDTO[]>([]);
 
-    const getData = async () => {
-        //오픈 API 호출
-        const API_URL = "https://api.unsplash.com/search/photos";
-        const API_KEY = "HfzO4u_yKo3IpSWxQeLCkwqJwrGwndrNQ2YnOocdrug";
-        const PER_PAGE = 30;
-
-        const searchValue = "Korea";
-        const pageValue = 100;
-
-        try{
-            const res = await axios.get( `${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`)
-            console.log(res);
-
-            if (res.status === 200) {
-                setImgUrls(res.data.results);
-            }
-
-        } catch (error) {
-            console.log()
-        }
-    }
-
-    const cardList = imgUrls.map((card: CardDTO) => {
+    const CARD_LIST = imgSelector.data.results.map((card: CardDTO) => {
         return (
             <Card data={card} key={card.id} />
         )
     });
-
-    useEffect(() => {
-        getData()
-    }, []);
 
     return (
     <div className={styles.page}>
@@ -65,7 +40,7 @@ function index() {
                 </div>
             </div>
             <div className={styles.page__contents__imageBox}>
-                {cardList}
+                {CARD_LIST}
             </div>
         </div>
         {/* {공통 푸터 UI 부분} */}
