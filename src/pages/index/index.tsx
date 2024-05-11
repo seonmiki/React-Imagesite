9b0966487bf559f6dddc151f0bf8158891fa0c65
seonmin <seonmiki@student.42seoul.kr>
2024-05-11 15:41:03 +0900

    Feat: 오픈 api 통신 및 호출

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -6,8 +6,32 @@ import CommonNav from "@/components/common/navigation/CommonNav";
 import CommonFooter from "@/components/common/footer/CommonFooter";
 import Card from "./components/Card"
 
+import axios from "axios";
+import { useEffect } from "react";
+
 function index() {
-  return (
+    const getData = async () => {
+        //오픈 API 호출
+        const API_URL = "https://api.unsplash.com/search/photos";
+        const API_KEY = "HfzO4u_yKo3IpSWxQeLCkwqJwrGwndrNQ2YnOocdrug";
+        const PER_PAGE = 30;
+
+        const searchValue = "Korea";
+        const pageValue = 100;
+
+        try{
+            const res = await axios.get( `${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`)
+            console.log(res);
+        } catch (error) {
+            console.log()
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    return (
     <div className={styles.page}>
 
         {/* {공통 헤더 UI 부분} */}
@@ -36,7 +60,7 @@ function index() {
         {/* {공통 푸터 UI 부분} */}
         <CommonFooter />
     </div>
-  )
+    )
 }
 
 export default index
