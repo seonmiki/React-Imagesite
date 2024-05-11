da71cbae0271b3e70456c85c1cf6741b08ed8510
seonmin <seonmiki@student.42seoul.kr>
2024-05-11 14:39:40 +0900

    Feat: card UI

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -4,6 +4,7 @@ import CommonHeader from "@/components/common/header/CommonHeader";
 import CommonSearchBar from "@/components/common/searchBar/CommonSearchBar";
 import CommonNav from "@/components/common/navigation/CommonNav";
 import CommonFooter from "@/components/common/footer/CommonFooter";
+import Card from "./components/Card"
 
 function index() {
   return (
@@ -25,7 +26,12 @@ function index() {
                     <CommonSearchBar />
                 </div>
             </div>
-            <div className={styles.page__contents__imageBox}></div>
+            <div className={styles.page__contents__imageBox}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
         {/* {공통 푸터 UI 부분} */}
         <CommonFooter />
